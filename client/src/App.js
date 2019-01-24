73f9ce5d0500ba41650bd543abb44ddb04f7d668
gala1234 <galadriel.calero@gig.com>
2019-01-24 16:21:33 +0100

    setup the back and the front

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        I'M READY TO THE BACK!
+      </div>
+    );
+  }
+}
+
+export default App;
